b53b4c49a0f15ef7c7f28736b8640bb76b9473df
Zesk García Rodríguez Salaberría <116458300+ZeskGarcia@users.noreply.github.com>
2022-11-04 17:00:11 +0100

    Update ConfigCL.lua

--- ConfigCL.lua
+++ ConfigCL.lua
@@ -1,5 +1,1 @@
-Config = {
-    DEBUG = false, -- Activate this if you want try the DEBUG mode.
-    PrintErrors = true, -- This will show you in the server console all the errors of the script in a print.
-    enableExports = true, -- This will active the exports['zeskoTextUIv1']:showTextUI(text, duration, theme) function for the client side scripts.
-}
+-- No needed
