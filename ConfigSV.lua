afbb91b83043f33e964521e11b0549f0126c5d66
Zesk García Rodríguez Salaberría <116458300+ZeskGarcia@users.noreply.github.com>
2022-11-04 16:59:55 +0100

    Update ConfigSV.lua

--- ConfigSV.lua
+++ ConfigSV.lua
@@ -1,4 +1,1 @@
-Config = {
-    ScriptVersion = "1.0.4", -- Don't touch it this can be an error in the server side that will appear if the function of below is enabled.
-    EnableVersionCheck = true, -- Activate this if you wan't to new the latest updates of this script.
-}
+-- No needed
