af11d1b4dccbd02642354495f9155c4c8f93afbb
Zesk García Rodríguez Salaberría <116458300+ZeskGarcia@users.noreply.github.com>
2022-11-06 10:35:46 +0100

    Update Server.lua

--- Server.lua
+++ Server.lua
@@ -1,1 +1,5 @@
 print(GetCurrentResourceName() .. " started");
+
+exports('showNewTextUI', function(text, timeouts, theme)
+    TriggerClientEvent('zeskoTextUi:client:show', source, text, timeouts, theme)
+end)
